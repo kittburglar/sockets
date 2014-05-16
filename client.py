61b5022ce7a1ae9f38e7adbe8fbb6e061e5a8e1a
Kittiphong Xayasane <kxayasan@gmail.com>
2014-05-15 22:35:38 -0400

    added files

---
+++ client.py
@@ -0,0 +1,11 @@
+from socket import * 
+serverName = 'localHost'
+serverPort = 12000 
+clientSocket = socket(AF_INET, SOCK_STREAM) 
+clientSocket.connect((serverName,serverPort)) 
+sentence = raw_input('Input lowercase sentence:') 
+clientSocket.send(sentence) 
+modifiedSentence = clientSocket.recv(1024) 
+print 'From Server:', modifiedSentence 
+clientSocket.close() 
+
