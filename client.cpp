99ea0a81ac6b9eb22c4232e66c09e3badcf08f9b
Kittiphong Xayasane <kxayasan@gmail.com>
2014-06-03 11:37:51 -0400

    added stuff

---
+++ client.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[])
+{
+	char serverName [20] = "servername";	
+	printf("%s",serverName);
+}
